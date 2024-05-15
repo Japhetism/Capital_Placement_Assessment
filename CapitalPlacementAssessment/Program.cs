9cd2b98abf6cc219281537e8f7a204883ff8cff7
Babatunde Ojo <babatundeojo30@gmail.com>
2024-05-15 13:48:37 +0100

    add cosmos db settings

--- CapitalPlacementAssessment/Program.cs
+++ CapitalPlacementAssessment/Program.cs
@@ -1,6 +1,31 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.Azure.Cosmos;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
+builder.Services.AddSingleton((provider) =>
+{
+    var endpointUri = configuration["CosmosDbSettings:EndpointUri"];
+    var primaryKey = configuration["CosmosDbSettings:PrimaryKey"];
+    var databaseName = configuration["CosmosDbSettings:DatabaseName"];
+
+    var cosmosClientOptions = new CosmosClientOptions
+    {
+        ApplicationName = databaseName
+    };
+
+    var loggerFactory = LoggerFactory.Create(builder =>
+    {
+        builder.AddConsole();
+    });
+
+    var cosmosClient = new CosmosClient(endpointUri, primaryKey, cosmosClientOptions);
+
+    cosmosClient.ClientOptions.ConnectionMode = ConnectionMode.Gateway;
+
+    return cosmosClient;
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
